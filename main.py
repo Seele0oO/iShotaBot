816b22fc9bba3ddcdfe42c4da35bac8fe036acbf
Xtao_dada <xtao@xtaolink.cn>
2021-12-13 21:10:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 from init import logs, user_me, bot
-def pre_init():
-    import os
-    anti_channel_file = "./data/anti_channel.pkl"
-    if os.path.exists(anti_channel_file):
-        return True
-    else:
-        os.mkdir("./data")
-        with open("./data/anti_channel.pkl","w") as pkl_file:
-            pass
+
+
 if __name__ == "__main__":
-    pre_init()
     logs.info(f"@{user_me.username} 运行成功！")
     bot.run()
